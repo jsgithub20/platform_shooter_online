492ed953ec0f00b786492123d278df4265199d22
jsgithub20 <niufortune@163.com>
2021-04-26 18:32:24 +0800

    from laptop

--- temp.py
+++ temp.py
@@ -1,50 +1,78 @@
-import asyncio, time
+import asyncio, logging
+
+# logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logging.basicConfig(format='\033[92m%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+# logging.addLevelName(logging.INFO, "\x1b[32m%s\x1b[32m" % logging.getLevelName(logging.INFO)) # change the color
 
 # test 1 starts here:
 # async def main():
-#     print(f'start: {time.strftime("%X")}')
+#     logging.info(f'starting')
 #     task = asyncio.create_task(foo('text'))
-#     print("task not started?")
-#     print("it seems task is not executed when created")
+#     # print("task not started?")
+#     # print("it seems task is not executed when created")
 #     await asyncio.sleep(1)
-#     print(f'main() sleep done: {time.strftime("%X")}')
+#     logging.info(f'main() sleep done')
 #     await task
-#     print(f'await task done: {time.strftime("%X")}')
+#     logging.info(f'await task done')
 #
 #
 # async def foo(text):
-#     print(f'foo() print(text) starting: {time.strftime("%X")}')
+#     logging.info(f'foo() print(text) starting')
 #     print(text)
-#     print(f'foo() sleep starting: {time.strftime("%X")}')
+#     logging.info(f'foo() sleep starting')
 #     await asyncio.sleep(3)
-#     print(f'foo() completed: {time.strftime("%X")}')
+#     logging.info(f'foo() completed')
 #
 # asyncio.run(main())
 # test 1 ends here.
 
 
 # test2 starts here:
-async def fetch_data():
-    print("start fetching")
-    await asyncio.sleep(2)
-    print("done fetching")
-    return {"data": 1}
+# async def fetch_data():
+#     logging.info("start fetching")
+#     await asyncio.sleep(2)
+#     logging.info("done fetching")
+#     return {"data": 1}
+#
+#
+# async def print_numbers():
+#     for i in range(10):
+#         print(i)
+#         if i == 3:
+#             logging.info(f"current task: {asyncio.current_task()}")
+#             # asyncio.current_task().cancel()
+#             return
+#         await asyncio.sleep(0.25)
+#
+#
+# async def main():
+#     task1 = asyncio.create_task(fetch_data())
+#     task2 = asyncio.create_task(print_numbers())
+#     logging.info(f"all tasks: {len(asyncio.all_tasks())}")
+#     value = await task1
+#     logging.warning(f"all tasks: {len(asyncio.all_tasks())}")
+#
+#     logging.info(f"fetched value: {value}")
+#
+#     await task2
+#     logging.info(f"all tasks: {len(asyncio.all_tasks())}")
+#
+#
+# asyncio.run(main())
+# test2 ends here
 
-
-async def print_numbers():
-    for i in range(10):
-        print(i)
-        await asyncio.sleep(0.25)
-
-
+# test3 starts here
 async def main():
-    task1 = asyncio.create_task(fetch_data())
-    task2 = asyncio.create_task(print_numbers())
+    print('tim')
+    task = asyncio.create_task(foo('text'))
+    print('finished')
 
-    value = await task1
-    print(value)
-    await task2
 
+async def foo(text):
+    # print(text)
+    await asyncio.sleep(3)
+    print(text)
 
 asyncio.run(main())
-# test2 ends here
+
+# test3 ends here
--- temp_server.py
+++ temp_server.py
@@ -1,57 +1,65 @@
-import asyncio, pygame, time
+import asyncio, pygame, time, logging
 
 pygame.init()
 clock = pygame.time.Clock()
 cnt = 0
 game_dict = {}
 
+# logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logging.basicConfig(format='\x1b[32m%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
 
 async def echo_server(reader, writer):
     global cnt, game_dict
     cnt += 1
-    print(f"Total connections: {cnt}")
+    logging.info(f"Total connections: {cnt}")
     writer.write(str(cnt).encode())
 
     game_id = (cnt - 1) // 2
     if cnt % 2 == 1:
+        task_id = 0
         game_dict[game_id] = (reader, writer)
         # print(f"game_dict[{game_id}] = {type(game_dict[game_id][-1])}")
     else:
+        task_id = 1
         game_dict[game_id+1] = (reader, writer)
         # print(f"game_dict[{game_id+1}] = {type(game_dict[game_id][-1])}")
 
     while True:
         # data = input("Msg to be sent to the clients: ").encode()  # Max number of bytes to read
         clock.tick(1)
+        # print(f"Active tasks: {len(asyncio.all_tasks())}")
         data = "server msg".encode()
         if not data:
             writer.close()
             break
         if cnt % 2 == 0:
-            if writer == game_dict[game_id][1]:
-                return "this task is done"
+            if task_id == 1:
+                logging.warning(f"Task 1 for game_id {game_id} is being returned")
+                return
             game_dict[game_id][1].write(data)
             await game_dict[game_id][1].drain()
             msg0 = await game_dict[game_id][0].read(100)
-            print(f"Received {time.strftime('%X')}: '{msg0.decode()}'")
+            logging.info(f"Received {time.strftime('%X')}: '{msg0.decode()}'")
             game_dict[game_id+1][1].write(data)
             await game_dict[game_id+1][1].drain()
             msg1 = await game_dict[game_id+1][0].read(100)
-            print(f"Received {time.strftime('%X')}: '{msg1.decode()}'")
+            logging.info(f"Received {time.strftime('%X')}: '{msg1.decode()}'")
         else:
             game_dict[game_id][1].write(data)
             await game_dict[game_id][1].drain()
             msg0 = await game_dict[game_id][0].read(100)
-            print(f"Received: '{msg0.decode()}'")
+            logging.info(f"Received: '{msg0.decode()}'")
 
 
 async def main(host, port):
     server = await asyncio.start_server(echo_server, host, port)
-    print(f"Server started with {host}:{port}")
+    # print(f"Server started with {host}:{port}")
+    logging.info(f"Server started at {host}:{port}")
     await server.serve_forever()
 
 
-asyncio.run(main('10.31.16.25', 5000))
+asyncio.run(main('192.168.3.18', 5000))
 
 # import asyncio
 #
