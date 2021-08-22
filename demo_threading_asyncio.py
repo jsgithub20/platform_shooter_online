9df223355776ec1da0f86fdad5f9936e1b227199
jsgithub20 <niufortune@163.com>
2021-08-22 18:16:45 +0800

    from laptop

--- demo_threading_asyncio.py
+++ demo_threading_asyncio.py
@@ -1,7 +1,6 @@
 import asyncio
-from concurrent.futures import CancelledError
+from concurrent.futures import CancelledError, TimeoutError
 from threading import Thread
-import datetime
 
 
 class EventLoop(Thread):
@@ -18,28 +17,41 @@ class EventLoop(Thread):
 
 
 async def some_func(number):
-    await asyncio.sleep(3)
+    await asyncio.sleep(number)
     return number
 
 
 event_loop = EventLoop()
-tasks = [event_loop.create_task(some_func(number)) for number in range(3)]
+# tasks = [event_loop.create_task(some_func(number)) for number in range(3)]
 
-# tasks[1].cancel()
+tasks = [event_loop.create_task(some_func(0)),
+         event_loop.create_task(some_func(1)),
+         event_loop.create_task(some_func(4))]
 
-cnt = 0
+tasks[1].cancel()
 
-while cnt < 3:
-    print("If you see this, the tasks are not blocking.")
-    for i, task in enumerate(tasks):
-        if task.done():
-            cnt += 1
-            print(f'Task {i} result: {task.result()}')
-
+# test case 1: to demonstrate timeout
 # for i, task in enumerate(tasks):
 #     try:
-#         print(f'Task {i} result: {task.result()}')
+#         print(f'Task {i} result: {task.result(1)}')
+#     except TimeoutError:
+#         print(f'Task {i} timeout')
 #     except CancelledError:
 #         print(f'Task {i} cancelled')
+# test case 1 ends here
+
+# test case 2: to demonstrate a way to run it without blocking
+cnt = 0
+while cnt < 3:
+    print("If you see this line, the tasks are not blocking")
+    for i, task in enumerate(tasks):
+        if task.done():  # task.done() is also true if the task is cancelled
+            cnt += 1
+            tasks.remove(task)
+            try:
+                print(f'Task {i} result: {task.result(1)}')
+            except CancelledError:
+                print(f'Task {i} is cancelled')
+# test case 2 ends here
 
 event_loop.stop()
